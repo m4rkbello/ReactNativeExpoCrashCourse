95d40a83732f1d6af886b995e1b5d3b03ec8550a
m4rkbello <markbello10041999@gmail.com>
2025-10-26 17:33:46 +0800

    FROM SCRATCH

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function TabsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Tabs Screen</Text>
+    </View>
+  );
+}
