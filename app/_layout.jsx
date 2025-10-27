93ebe2373d208b469b941a1627d6cac30b56e0be
m4rkbello <markbello10041999@gmail.com>
2025-10-27 21:32:48 +0800

    POMODORO - LAYOUT AND STACK

---
+++ app/_layout.jsx
@@ -0,0 +1,19 @@
+import { Stack } from 'expo-router'
+import { StyleSheet } from 'react-native'
+
+const RootLayout = () => {
+  return (
+        <Stack screenOptions={{
+            headerStyle: { backgroundColor: '#ddd'},
+            headerTintColor: '#333'
+        }}>
+            <Stack.Screen name="index" options={{title: 'Home'}} />
+            <Stack.Screen name="about" options={{title: 'About'}} />
+            <Stack.Screen name="contact" options={{title: 'Contact', headerShown: false,}} />
+        </Stack>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
