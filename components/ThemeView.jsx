172a4b55dd20ccad5eaf5040f69db72ab1283246
m4rkbello <markbello10041999@gmail.com>
2025-10-28 22:12:27 +0800

    ADDED

--- components/ThemeView.jsx
+++ components/ThemeView.jsx
@@ -6,7 +6,6 @@ const ThemeView = ({style, ...props}) => {
 const colorScheme = useColorScheme()
 const theme = Colors[colorScheme] ?? Colors.light
 
-
   return (
     <View 
         style={[{backgroundColor: theme.background}, style]}
