3f01b3962e658371c39f1fcfc41e11550be8527c
m4rkbello <markbello10041999@gmail.com>
2025-10-28 22:12:31 +0800

    ADDED

--- constants/theme1.jsx
+++ constants/theme1.jsx
@@ -6,6 +6,7 @@ export const Colors = {
   light: {
     text: '#11181C',
     background: '#fff',
+    uiBackground: '#c5c5c5ff',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
@@ -14,6 +15,7 @@ export const Colors = {
   dark: {
     text: '#ECEDEE',
     background: '#151718',
+      uiBackground: '#c5c5c5ff',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
