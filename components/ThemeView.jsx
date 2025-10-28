13115f89e686625a3fc9e4edb4dc48b96fc934de
m4rkbello <markbello10041999@gmail.com>
2025-10-28 21:35:39 +0800

    ADDED

---
+++ components/ThemeView.jsx
@@ -0,0 +1,20 @@
+import { View, useColorScheme } from 'react-native';
+import { Colors } from '../constants/theme1';
+
+const ThemeView = ({style, ...props}) => {
+
+const colorScheme = useColorScheme()
+const theme = Colors[colorScheme] ?? Colors.light
+
+
+  return (
+    <View 
+        style={[{backgroundColor: theme.background}, style]}
+        {...props}
+    />
+     
+  )
+}
+
+export default ThemeView
+
