34365b3f2426f4293a1f93d0593419599cbe5b7c
m4rkbello <markbello10041999@gmail.com>
2025-10-28 22:12:23 +0800

    ADDED

---
+++ components/ThemeLogo.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+//images
+
+
+const ThemeLogo = () => {
+  return (
+    <View>
+      <Text>ThemeLogo</Text>
+    </View>
+  )
+}
+
+export default ThemeLogo
+
+const styles = StyleSheet.create({})
